d8744875db591507df8f85f5d8e8fa9b37403b19
Xiovera <rocioyovera@gmail.com>
2017-10-26 14:07:08 -0700

    iniciando

---
+++ assets/js/main.js
@@ -0,0 +1,9 @@
+function changeColor1() {
+    document.getElementById("background").style.backgroundColor = "red";
+}
+function changeColor2() {
+    document.getElementById("background").style.backgroundColor = "blue";
+}
+function changeColor3() {
+    document.getElementById("background").style.backgroundColor= "yellow";
+}
