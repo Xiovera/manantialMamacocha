1722791cb7e2ff6dfaaad48ad848e8647295f1a9
Xiovera <rocioyovera@gmail.com>
2017-10-27 10:29:17 -0700

    avance

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,9 +1,1 @@
-function changeColor1() {
-    document.getElementById("background").style.backgroundColor = "red";
-}
-function changeColor2() {
-    document.getElementById("background").style.backgroundColor = "blue";
-}
-function changeColor3() {
-    document.getElementById("background").style.backgroundColor= "yellow";
-}
+  $('.carousel.carousel-slider').carousel({fullWidth: true});
